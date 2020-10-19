1807b5beb5255aa1d8e52a32841e4edfd36977a1
Masafumi Sekikawa <sekimasa10@yahoo.co.jp>
2020-10-19 21:51:19 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Container, container } from 'reactstrap';
+import { Container, Button, Table } from 'reactstrap';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import TodosContextProvoider from './contexts/TodoContext';
 
 function App() {
+
   return (
     <div className="App">
       <Container>
-        <h1>Todo</h1>
+        <h1 className="mt-4">Todo リスト</h1>
+        <TodosContextProvoider>
+        <TodoForm />
+        <TodoList />
+        </TodosContextProvoider>
       </Container>
     </div>
   );
